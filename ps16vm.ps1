0a722f839047bcdd2a15b925a66fa0e7fe053b49
Noah Scholz <48360809+noah1400@users.noreply.github.com>
2024-03-28 15:14:49 +0100

    isPrime routine

--- ps16vm.ps1
+++ ps16vm.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 $IP = 0
 $SP = 1
 $FP = 2
@@ -37,6 +39,7 @@ $OP_XOR = 9 # XOR R0, R1 -> R0 = R0 ^ R1
 $OP_SHL = 10 # SHL R0, R1 -> R0 = R0 << R1
 $OP_SHR = 11 # SHR R0, R1 -> R0 = R0 >> R1
 $OP_CMP = 12 # CMP R0, R1 -> ACC = R0 - R1
+$OP_CMP_IMM = 120 # CMP R0, 0x0000000000000000 -> ACC = R0 - 0x0000000000000000
 $OP_JMP = 13 # JMP 0x0000000000000000
 $OP_JE = 14 # JE 0x0000000000000000
 $OP_JNE = 15 # JNE 0x0000000000000000
@@ -47,6 +50,46 @@ $OP_RET = 19 # RET
 $OP_PUSH = 20 # PUSH R0
 $OP_POP = 21 # POP R0
 $OP_HLT = 22 # HLT
+$OP_INC = 23 # INC R0
+$OP_DEC = 24 # DEC R0
+
+function Map-Op-Code-To-String {
+    param (
+        [uint16]$op
+    )
+    switch ($op) {
+        $OP_NOP { return 'NOP' }
+        $OP_MOV { return 'MOV' }
+        $OP_MOV_MEM { return 'MOV_MEM' }
+        $OP_MOV_MEM_R { return 'MOV_MEM_R' }
+        $OP_MOV_IMM { return 'MOV_IMM' }
+        $OP_ADD { return 'ADD' }
+        $OP_SUB { return 'SUB' }
+        $OP_MUL { return 'MUL' }
+        $OP_DIV { return 'DIV' }
+        $OP_MOD { return 'MOD' }
+        $OP_AND { return 'AND' }
+        $OP_OR { return 'OR' }
+        $OP_XOR { return 'XOR' }
+        $OP_SHL { return 'SHL' }
+        $OP_SHR { return 'SHR' }
+        $OP_CMP { return 'CMP' }
+        $OP_CMP_IMM { return 'CMP_IMM' }
+        $OP_JMP { return 'JMP' }
+        $OP_JE { return 'JE' }
+        $OP_JNE { return 'JNE' }
+        $OP_JG { return 'JG' }
+        $OP_JL { return 'JL' }
+        $OP_CALL { return 'CALL' }
+        $OP_RET { return 'RET' }
+        $OP_PUSH { return 'PUSH' }
+        $OP_POP { return 'POP' }
+        $OP_HLT { return 'HLT' }
+        $OP_INC { return 'INC' }
+        $OP_DEC { return 'DEC' }
+        default { return 'UNKNOWN' }
+    }
+}
 
 function New-Mem {
     param (
@@ -227,24 +270,8 @@ function New-CPU {
     $cpu = New-Object PSObject
     $cpu | Add-Member -MemberType NoteProperty -Name mm -Value (New-MM)
     $cpu | Add-Member -MemberType NoteProperty -Name registers -Value (New-Object 'uint16[]' 20)
+    $cpu | Add-Member -MemberType NoteProperty -Name stackframeSize -Value 0
     return $cpu
-}
-
-function Dereference-Reg {
-    param (
-        [object]$cpu,
-        [uint16]$index
-    )
-    return Fetch16 -mm $cpu.mm -address $cpu.registers[$index]
-}
-
-function Store-At-Reg {
-    param (
-        [object]$cpu,
-        [uint16]$index,
-        [uint16]$value
-    )
-    $cpu.registers[$index] = $value
 }
 
 function Push-Stack {
@@ -253,8 +280,8 @@ function Push-Stack {
         [uint16]$value
     )
     $cpu.registers[$SP] -= 2
-    Write-Host ('Pushing 0x{0:X16} to 0x{1:X16}' -f $value, $cpu.registers[$SP])
     Store16 -mm $cpu.mm -address $cpu.registers[$SP] -value $value
+    $cpu.stackframeSize += 2
 }
 
 function Pop-Stack {
@@ -263,7 +290,70 @@ function Pop-Stack {
     )
     $value = Fetch16 -mm $cpu.mm -address $cpu.registers[$SP]
     $cpu.registers[$SP] += 2
+    $cpu.stackframeSize -= 2
     return $value
+}
+
+
+function Push-State {
+    param (
+        [object]$cpu
+    )
+    # push all registers beginning from R0 to R15
+    Push-Stack -cpu $cpu -value $cpu.registers[$R0]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R1]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R2]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R3]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R4]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R5]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R6]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R7]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R8]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R9]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R10]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R11]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R12]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R13]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R14]
+    Push-Stack -cpu $cpu -value $cpu.registers[$R15]
+    Push-Stack -cpu $cpu -value $cpu.registers[$IP]
+    # push stackframe size + 2 (2 bytes)
+    Push-Stack -cpu $cpu -value ($cpu.stackframeSize + 2)
+    # set frame pointer to stack pointer
+    $cpu.registers[$FP] = $cpu.registers[$SP]
+    # set stackframe size to 0
+    $cpu.stackframeSize = 0
+}
+
+function Pop-State {
+    param (
+        [object]$cpu
+    )
+    $fpa = $cpu.registers[$FP]
+    $cpu.registers[$SP] = $fpa
+    $cpu.stackframeSize = Pop-Stack -cpu $cpu
+    $sfs = $cpu.stackframeSize
+    # pop IP
+    $cpu.registers[$IP] = Pop-Stack -cpu $cpu
+    # pop all registers beginning from R15 to R0
+    $cpu.registers[$R15] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R14] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R13] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R12] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R11] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R10] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R9] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R8] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R7] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R6] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R5] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R4] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R3] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R2] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R1] = Pop-Stack -cpu $cpu
+    $cpu.registers[$R0] = Pop-Stack -cpu $cpu
+
+    $cpu.registers[$FP] = $fpa + $sfs
 }
 
 function Dump-Mem {
@@ -412,7 +502,20 @@ function Run-Step {
         $OP_CMP {
             $r1_index = Fetch8-CPU -cpu $cpu
             $r2_index = Fetch8-CPU -cpu $cpu
-            $cpu.registers[$ACC] = $cpu.registers[$r1_index] - $cpu.registers[$r2_index]
+            # convert to signed integer
+            $v1 = [int16]$cpu.registers[$r1_index]
+            $v2 = [int16]$cpu.registers[$r2_index]
+            $result = $v1 - $v2
+            $result = $result -band 0xFFFF
+            $result = [convert]::ToUInt16($result)
+            $cpu.registers[$ACC] = $result
+        }
+        $OP_CMP_IMM {
+            $r_index = Fetch8-CPU -cpu $cpu
+            $value = [int16](Fetch16-CPU -cpu $cpu)
+            $result = $cpu.registers[$r_index] - $value
+            $result = $result -band 0xFFFF
+            $cpu.registers[$ACC] = $result
         }
         $OP_JMP {
             $address = Fetch16-CPU -cpu $cpu
@@ -432,24 +535,38 @@ function Run-Step {
         }
         $OP_JG {
             $address = Fetch16-CPU -cpu $cpu
-            if ($cpu.registers[$ACC] -gt 0) {
+
+            $v1_temp = [int32]$cpu.registers[$ACC]
+
+            if ($v1_temp -gt 32767) {
+                $v1_temp = $v1_temp - 65536
+            }
+            $v1 = [int16]$v1_temp
+
+            if ($v1 -gt 0) {
                 $cpu.registers[$IP] = $address
             }
         }
         $OP_JL {
             $address = Fetch16-CPU -cpu $cpu
-            if ($cpu.registers[$ACC] -lt 0) {
+            # convert to signed integer
+            $v1 = [int32]$cpu.registers[$ACC]
+
+            if ($v1 -gt 32767) {
+                $v1 = $v1 - 65536
+            }
+            $v1 = [int16]$v1
+            if ($v1 -lt 0) {
                 $cpu.registers[$IP] = $address
             }
         }
         $OP_CALL {
             $address = Fetch16-CPU -cpu $cpu
-            Push-Stack -cpu $cpu -value $cpu.registers[$IP]
+            Push-State -cpu $cpu 
             $cpu.registers[$IP] = $address
         }
         $OP_RET {
-            $address = Pop-Stack -cpu $cpu
-            $cpu.registers[$IP] = $address
+            Pop-State -cpu $cpu
         }
         $OP_PUSH {
             $r_index = Fetch8-CPU -cpu $cpu
@@ -459,8 +576,20 @@ function Run-Step {
             $r_index = Fetch8-CPU -cpu $cpu
             $cpu.registers[$r_index] = Pop-Stack -cpu $cpu
         }
+        $OP_INC {
+            $r_index = Fetch8-CPU -cpu $cpu
+            $cpu.registers[$r_index]++
+        }
+        $OP_DEC {
+            $r_index = Fetch8-CPU -cpu $cpu
+            $cpu.registers[$r_index]--
+        }
         $OP_HLT {
-            # Halt
+            return $OP_HLT
+        }
+        default {
+            Write-Host ('Unknown OP code: {0:X2}' -f $op)
+            return $OP_HLT
         }
     }
     return $op
@@ -478,20 +607,46 @@ function Main {
     $cpu.registers[$IP] = 0
     $cpu.registers[$SP] = $STACK_BASE
 
-    # Flash memory
-    Write-Mem -mem $mem -start 0 -data @(
-        $OP_MOV_IMM, $R0, 0x01, 0x00,
-        $OP_MOV_IMM, $R1, 0x0F, 0x00,
-        $OP_ADD, $R0, $R1,
-        $OP_ADD, $R2, $R1,
-        $OP_PUSH, $R0,
-        $OP_PUSH, $R1,
-        $OP_PUSH, $R2,
-        $OP_POP, $R3,
-        $OP_POP, $R4,
-        $OP_POP, $R5,
-        $OP_JMP, 0x00, 0x00,
+    # Main routine
+    Write-Mem -m $mem -start 0 -data @(
+        $OP_MOV_IMM, $R0, 0x61, 0x00, # R0 = 0x0061 (97)
+        $OP_CALL, 0x00, 0x01, # CALL 0x0100 (is_prime)
+        $OP_MOV, $R1, $ACC, # R1 = ACC
         $OP_HLT
+    )
+
+    # is_prime routine
+    Write-Mem -m $mem -start 0x0100 -data @(
+        $OP_CMP_IMM, $R0, 0x02, 0x00, # CMP R0, 0x0002
+        $OP_JL, 0x00, 0x1A, # JL 0x1A00 (not_prime) r0 < 2 -> not prime
+        $OP_JE, 0x00, 0x1C, # JE 0x1C00 (prime) r0 == 2 -> prime
+        $OP_MOV_IMM, $R1, 0x02, 0x00, # R1 = 0x0002
+        $OP_JMP, 0x00, 0x05 # JMP 0x0500 (loop)
+    )
+
+    # loop routine
+    Write-Mem -m $mem -start 0x0500 -data @(
+        $OP_MOV, $R2, $R0, # R2 = R0
+        $OP_MOD, $R2, $R1, # R2 = R2 % R1
+        $OP_CMP_IMM, $R2, 0x00, 0x00, # CMP R2, 0x0000
+        $OP_JE, 0x00, 0x1A, # JE 0x1A00 (not_prime) r2 == 0 -> not prime
+        $OP_INC, $R1, # R1++
+        $OP_CMP, $R1, $R0, # CMP R1, R0
+        $OP_JG, 0x00, 0x1C, # JG 0x1C00 (prime) r1 > r0 -> prime
+        $OP_JE, 0x00, 0x1C, # JE 0x1C00 (prime) r1 == r0 -> prime
+        $OP_JMP, 0x00, 0x05 # JMP 0x0500 (loop)
+    )
+
+    # not_prime routine
+    Write-Mem -m $mem -start 0x1A00 -data @(
+        $OP_MOV_IMM, $ACC, 0x00, 0x00, # ACC = 0x0000
+        $OP_RET
+    )
+
+    # prime routine
+    Write-Mem -m $mem -start 0x1C00 -data @(
+        $OP_MOV_IMM, $ACC, 0x01, 0x00, # ACC = 0x0001
+        $OP_RET
     )
 
     # Dump memory
@@ -500,14 +655,19 @@ function Main {
 
     # Run
     while (($op = Run-Step -cpu $cpu) -ne $OP_HLT) {
-        Write-Host ('OP: {0:X2}' -f $op)
-        Dump-Reg -cpu $cpu
-        Dump-Mem -mem $mem -start ($cpu.registers[$SP]) -end $STACK_BASE
-        Read-Host 'Press Enter to continue'
+        # Write-Host ('Prev. OP: {0:X2} : {1}' -f $op ,(Map-Op-Code-To-String -op $op))
+        # Dump-Reg -cpu $cpu
+        # Write-Host "Stack:"
+        # Dump-Mem -mem $mem -start ($cpu.registers[$SP]) -end $STACK_BASE
+        # Write-Host "Memory at IP:"
+        # Dump-Mem -mem $mem -start $cpu.registers[$IP] -end ($cpu.registers[$IP] + 0x30)
+        # Read-Host 'Press Enter to continue'
     }
 
+    # Dump registers
+    Dump-Reg -cpu $cpu
     # Dump memory
-    Dump-Mem -mem $mem -start 0 -end 0x0010
+    Dump-Mem -mem $mem -start $cpu.registers[$IP] -end ($cpu.registers[$IP] + 0x30)
 }
 
 Main
